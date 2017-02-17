19f1011fe4dd446ec02274ba3fdec65f8652fbb0
Dmitry Nekrasov <scannorone@gmail.com>
2017-02-17 17:39:20 +0700

    Обработчик чтения данных с устройства

--- AVL-Tree/avl-tree.c
+++ AVL-Tree/avl-tree.c
@@ -8,6 +8,8 @@
 #define DEV_MINOR 0
 #define DEV_NAME "avl_tree"
 
+#define MAXSTR 100
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("dmitry");
 MODULE_DESCRIPTION("Character device driver AVL tree");
@@ -15,7 +17,7 @@ MODULE_DESCRIPTION("Character device driver AVL tree");
 static int dev_open(struct inode*, struct file*);
 static int dev_rls(struct inode*, struct file*);
 static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
-static ssize_t dev_write(struct file*, const char*, size_t, loff_t);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
 
 static struct file_operations fops =
 {
@@ -25,6 +27,7 @@ static struct file_operations fops =
 	.release = dev_rls
 };
 
+static char msg[MAXSTR] = { 0 };
 static int times = 0;
 
 int init_module(void)
@@ -51,5 +54,23 @@ static int dev_open(struct inode* my_inode, struct file* my_file)
 	printk(KERN_ALERT "Device opened %d times\n", times);
 
 	return 0;
+}
+
+static ssize_t dev_read(struct file* my_file, char* buff, size_t len, loff_t* off)
+{
+	int msg_len = strlen(msg);
+	int size = len >= msg_len ? msg_len : len;
+
+	if (*off >= msg_len) {
+		return 0;
+	}
+
+	if (copy_to_user(buff, msg, size)) {
+		return -EFAULT;
+	}
+
+	*off += size;
+
+	return len;
 }
 
