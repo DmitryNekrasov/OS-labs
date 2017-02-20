90961b10237641612374118e3c949a9eae3974b1
Dmitry Nekrasov <scannorone@gmail.com>
2017-02-20 11:39:39 +0700

    Добавил проект integral

---
+++ integral/integral.c
@@ -0,0 +1,107 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/string.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+
+#define DEV_MAJOR 105
+#define DEV_MINOR 0
+#define DEV_NAME "integral"
+
+#define MAXSTR 100
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("dmitry");
+MODULE_DESCRIPTION("Trapezoidal rule for approximating the definite integral");
+
+static int dev_open(struct inode*, struct file*);
+static int dev_rls(struct inode*, struct file*);
+static ssize_t dev_read(struct file*, char*, size_t, loff_t*);
+static ssize_t dev_write(struct file*, const char*, size_t, loff_t*);
+
+static struct file_operations fops =
+{
+	.read = dev_read,
+	.open = dev_open,
+	.write = dev_write,
+	.release = dev_rls
+};
+
+static char msg[MAXSTR] = { 0 };
+static int times = 0;
+
+static void process(void)
+{
+	char hi[MAXSTR] = "Hello, ";
+	strncpy(msg, strcat(hi, msg), MAXSTR);
+	strncpy(msg, strcat(msg, "\n"), MAXSTR);
+}
+
+int init_module(void)
+{
+	int t = register_chrdev(DEV_MAJOR, DEV_NAME, &fops);
+
+	if (t < 0) {
+		printk(KERN_ALERT "Device registration failed\n");
+	} else {
+		printk(KERN_ALERT "Device registered\n");
+	}
+
+	return t;
+}
+
+void cleanup_module(void)
+{
+	unregister_chrdev(DEV_MAJOR, DEV_NAME);
+}
+
+static int dev_open(struct inode* my_inode, struct file* my_file)
+{
+	times++;
+	printk(KERN_ALERT "Device opened %d times\n", times);
+
+	return 0;
+}
+
+static int dev_rls(struct inode* my_inode, struct file* my_file)
+{
+	printk(KERN_ALERT "Device for integral closed\n");
+	return 0;
+}
+
+static ssize_t dev_read(struct file* my_file, char* buff, size_t len, loff_t* off)
+{
+	int msg_len = strlen(msg);
+	int size = len >= msg_len ? msg_len : len;
+
+	if (*off >= msg_len) {
+		return 0;
+	}
+
+	if (copy_to_user(buff, msg, size)) {
+		return -EFAULT;
+	}
+
+	*off += size;
+
+	return len;
+}
+
+static ssize_t dev_write(struct file* my_file, const char* buff, size_t len, loff_t* off)
+{
+	unsigned long ret;
+	printk(KERN_INFO "dev write\n");
+
+	if (len > sizeof(msg) - 1) {
+		return -EINVAL;
+	}
+
+	ret = copy_from_user(msg, buff, len);
+	if (ret) {
+		return -EFAULT;
+	}
+
+	msg[len] = '\0';
+	process();
+	return len;
+}
